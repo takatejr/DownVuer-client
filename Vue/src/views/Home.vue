3249c0cd6b35145c2fa6fb711c2208f850201895
takatejr <58137383+takatejr@users.noreply.github.com>
2021-02-21 18:46:23 +0100

    Delete Vue directory

--- Vue/src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1 class="title columns is-centered is-mobile">DownVuer</h1>
-  <div class="home columns is-centered is-mobile">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
-</template>
--- Vue/src/views/Spotify.vue
+++
@@ -1,5 +0,0 @@
-// Select user >> select public list >> download as...
-
-<template>
-    <div class="button is-primary">Spotify</div>
-</template>
